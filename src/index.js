f476e21f45b5d817f540de3884ff5171e5e8191b
Ryan Ottney <rottney123@gmail.com>
2021-02-17 21:48:50 -0800

    started >2player functionality, need to learn keys

--- src/index.js
+++ src/index.js
@@ -14,25 +14,20 @@ function Square(props) {
 class Board extends React.Component {
   constructor(props) {
     super(props);
+    const NUM_PLAYERS = 2;  // will be provided...
     this.state = {
-      squares: Array(14).fill(null),
-      player1Score: 0,
-      player2Score: 0,
-      player1ClosedAll: false,
-      player2ClosedAll: false,
-      player1Won: false,
-      player2Won: false,
+      squares: Array(7*NUM_PLAYERS).fill(null),
+      scores: Array(NUM_PLAYERS).fill(0),
+      closedAll: Array(NUM_PLAYERS).fill(false),
+      won: Array(NUM_PLAYERS).fill(false),
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    let player1Score = this.state.player1Score;
-    let player2Score = this.state.player2Score;
-    let player1ClosedAll = this.state.player1ClosedAll;
-    let player2ClosedAll = this.state.player2ClosedAll;
-    let player1Won = this.state.player1Won;
-    let player2Won = this.state.player2Won;
+    let scores = this.state.scores;
+    let closedAll = this.state.closedAll;
+    let won = this.state.won;
 
     if (this.state.squares[i] === null) {
       squares[i] = "/";
@@ -45,46 +40,46 @@ class Board extends React.Component {
     }
     else {
       if (i === 0 && squares[i + 1] !== "Ⓧ") {
-        player1Score += 20;
+        scores[0] += 20;
       }
       else if (i === 1 && squares[i - 1] !== "Ⓧ") {
-        player2Score += 20;
+        scores[1] += 20;
       }
       else if (i === 2 && squares[i + 1] !== "Ⓧ") {
-        player1Score += 19;
+        scores[0] += 19;
       }
       else if (i === 3 && squares[i - 1] !== "Ⓧ") {
-        player2Score += 19;
+        scores[1] += 19;
       }
       else if (i === 4 && squares[i + 1] !== "Ⓧ") {
-        player1Score += 18;
+        scores[0] += 18;
       }
       else if (i === 5 && squares[i - 1] !== "Ⓧ") {
-        player2Score += 18;
+        scores[1] += 18;
       }
       else if (i === 6 && squares[i + 1] !== "Ⓧ") {
-        player1Score += 17;
+        scores[0] += 17;
       }
       else if (i === 7 && squares[i - 1] !== "Ⓧ") {
-        player2Score += 17;
+        scores[1] += 17;
       }
       else if (i === 8 && squares[i + 1] !== "Ⓧ") {
-        player1Score += 16;
+        scores[0] += 16;
       }
       else if (i === 9 && squares[i - 1] !== "Ⓧ") {
-        player2Score += 16;
+        scores[1] += 16;
       }
       else if (i === 10 && squares[i + 1] !== "Ⓧ") {
-        player1Score += 15;
+        scores[0] += 15;
       }
       else if (i === 11 && squares[i - 1] !== "Ⓧ") {
-        player2Score += 15;
+        scores[1] += 15;
       }
       else if (i === 12 && squares[i + 1] !== "Ⓧ") {
-        player1Score += 25;
+        scores[0] += 25;
       }
       else if (i === 13 && squares[i - 1] !== "Ⓧ") {
-        player2Score += 25;
+        scores[1] += 25;
       }
       else {
         console.log("Invalid cell clicked...how is this even possible??")
@@ -92,18 +87,18 @@ class Board extends React.Component {
     }
 
     // Probably refactor this...
-    player1ClosedAll = true;
+    closedAll[0] = true;
     for (let i = 0; i <= 12; i += 2) {
       if (squares[i] !== "Ⓧ") {
-        player1ClosedAll = false;
+        closedAll[0] = false;
         i = 12;
       }
     }
 
-    player2ClosedAll = true;
+    closedAll[1] = true;
     for (let i = 1; i <= 13; i += 2) {
       if (squares[i] !== "Ⓧ") {
-        player2ClosedAll = false;
+        closedAll[1] = false;
         i = 13;
       }
     }
@@ -111,12 +106,9 @@ class Board extends React.Component {
     this.setState(
       {
         squares: squares,
-        player1Score: player1Score,
-        player2Score: player2Score,
-        player1ClosedAll: player1ClosedAll,
-        player2ClosedAll: player2ClosedAll,
-        player1Won: player1Won,
-        player2Won: player2Won,
+        scores: scores,
+        closedAll: closedAll,
+        won: won,
       }
     );
   }
@@ -170,13 +162,14 @@ class Board extends React.Component {
 
     // Generate scores
     let scores = [];
-    scores.push(<div className="score">{this.state.player1Score}</div>);
-    scores.push(<div className="score">{this.state.player2Score}</div>);
+    for (let i = 0; i < NUM_PLAYERS; i++) {
+      scores.push(<div className="score">{this.state.scores[i]}</div>)
+    }
 
     // Determine winner
     const winner = (
       <div className="status">
-        {declareWinner(this.state.player1ClosedAll, this.state.player2ClosedAll, this.state.player1Score, this.state.player2Score)}
+        {declareWinner(this.state.closedAll, this.state.scores)}
       </div>
     );
 
@@ -215,12 +208,14 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-function declareWinner(player1ClosedAll, player2ClosedAll, player1Score, player2Score) {
-  if (player1ClosedAll === true && player1Score >= player2Score) {
+
+function declareWinner(closedAll, scores) {
+  // ONLY WORKS FOR 2 PLAYERS NOW
+  if (closedAll[0] && scores[0] >= scores[1]) {
     return "Player 1 wins.";
   }
 
-  if (player2ClosedAll === true && player2Score >= player1Score) {
+  if (closedAll[1] && scores[1] >= scores[0]) {
     return "Player 2 wins.";
   }
 
